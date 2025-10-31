bfe3a751419bd8041eebb15f46911c8f60f78bd4
Yash Singh <yashsingh34609@gmail.com>
2025-10-31 06:34:19 +0000

    auto-commit for 7b9ab999-4481-44a4-963f-8d0ba8b6d6c3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AdminPage from "./pages/AdminPage";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </BrowserRouter>
     </div>
